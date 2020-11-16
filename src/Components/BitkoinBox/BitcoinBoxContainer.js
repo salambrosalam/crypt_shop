2681d6d236cb2de0b073d2c8c8125864c83e3021
maxikgreat <maximvasylenko228322@gmail.com>
2020-11-16 17:38:30 +0100

    Code minified

--- src/Components/BitkoinBox/BitcoinBoxContainer.js
+++ src/Components/BitkoinBox/BitcoinBoxContainer.js
@@ -4,30 +4,19 @@ import {getBitcoinPriceTC} from "../../Redux/reducers/AppReducer";
 import BitcoinBox from "./BitcoinBox";
 
 const BitcoinBoxContainer = (props) => {
-
-    const [bitcoinUSDCurrency, setBitcoinUSDCurrency] = useState();
-    const [bitcoinEURCurrency, setBitcoinEURCurrency] = useState();
-    const [bitcoinGBPCurrency, setBitcoinGBPCurrency] = useState();
-
+    const stateBitcoin = useSelector(state => state.menu.bitcoinPrice);
     const dispatch = useDispatch();
+
     useEffect(() => {
-        dispatch(getBitcoinPriceTC())
-        if (state.bitcoinPrice) {
-            let bitcoinPriceUSD = state.bitcoinPrice.bpi.USD.rate;
-            let bitcoinPriceEUR = state.bitcoinPrice.bpi.EUR.rate;
-            let bitcoinPriceGBP = state.bitcoinPrice.bpi.GBP.rate;
-            setBitcoinUSDCurrency(bitcoinPriceUSD);
-            setBitcoinEURCurrency(bitcoinPriceEUR);
-            setBitcoinGBPCurrency(bitcoinPriceGBP);
-        }
-    })
+      const timer = setInterval(() => dispatch(getBitcoinPriceTC()), 3000);
+      return () => clearInterval(timer); 
+    }, []);
 
-    const state = useSelector(state => state.menu)
-
-    return (
-        <BitcoinBox bitcoinUSD={bitcoinUSDCurrency}
-                    bitcoinEUR={bitcoinEURCurrency}
-                    bitcoinGBP={bitcoinGBPCurrency}/>
+    return stateBitcoin && (
+      <BitcoinBox bitcoinUSD={stateBitcoin.bpi.USD.rate}
+        bitcoinEUR={stateBitcoin.bpi.EUR.rate}
+        bitcoinGBP={stateBitcoin.bpi.USD.rate}
+      />
     )
 }
 
